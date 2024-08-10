bda87df2312cb9c5ac094f02c35d2d947104580a
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-08-10 19:15:43 +0900

    [Gold IV] Title: N-Queen, Time: 4504 ms, Memory: 119888 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,46 +1,41 @@
-# 좌상 대각선과 우상 대각선 확인 해야함.
-def diag(r, c):
-    # 좌상 대각선 => r-x,c-x -> x: 0~ n-1  & 범위 이탈 여부 확인
-    for x in range(1, n):
-        if 0 <= r - x and c - x >= 0 and arr[r - x][c - x] == 1:
-            return False
-    # 우상 대각선 => r-x,c+x -> x: 0~ n-1 & 범위 이탈 여부 확인
-    for x in range(1, n):
-        if 0 <= r - x and c + x < n and arr[r - x][c + x] == 1:
-            return False
+n = int(input())
 
-    # 그외
-    return True
+# 세로행 방문 여부
+vertical = [0] * (n)
 
-# back-tracking function
-def recur(level):
-    global cnt
-    if level == n: #  full-placed
-        cnt += 1
+# 좌상 대각선 ( 행-열 : -(n-1)~ n-1)
+# => n-1을 강제로 더해버리면 됨  0~ 2n-2
+left_diagonal = [0] * (2 * n - 1)
+
+# 우상 대각선 (행 + 열 : 0~ 2n-2)
+right_diagonal = [0] * (2 * n - 1)
+
+count = 0
+
+
+def backtrack(level):
+    global count
+
+    if level == n:
+        count += 1
         return
 
-    # place the queen
-    for col in range(n):
-        # can place?
-        if not col_visited[col] and diag(level,col):
-            col_visited[col] = 1
-            arr[level][col] = 1
-            recur(level+1)
-            col_visited[col] = 0
-            arr[level][col] = 0
+    # 재귀 호출
+    for idx in range(n):
+        if vertical[idx] or left_diagonal[level - idx + (n - 1)] or right_diagonal[level + idx]:
+            continue
+        # 다음 방문
+        vertical[idx] = 1
+        left_diagonal[level - idx + (n - 1)] = 1
+        right_diagonal[level + idx] = 1
 
-n = int(input())
-arr = [ [0]*n for _ in range(n)]
+        backtrack(level + 1)
 
-# 각 row 마다 위에서부터 하나씩 놓으면 됨
-# 각 col 마다 겹치면 안됨 -> col_lst = []
-col_visited = [0] * n
+        # 복구
+        vertical[idx] = 0
+        left_diagonal[level - idx + (n - 1)] = 0
+        right_diagonal[level + idx] = 0
 
-# count
-cnt =0
 
-# call the function
-recur(0)
-
-# print
-print(cnt)
+backtrack(0)
+print(count)
