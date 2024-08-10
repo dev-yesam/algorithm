3674af43c9e2cac27df7f0aa843e5551f7b2d3cf
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-08-10 20:29:43 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,35 +3,23 @@ from collections import deque
 n, m = map(int, input().split())
 arr = [list(input()) for _ in range(n)]
 
-depth = [[0] * m for _ in range(n)]
 
-# 델타 상하 좌우
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
+def bfs():
+    queue = deque()
+    queue.append((0, 0))
+    visited = [[0] * m for _ in range(n)]
+    visited[0][0] = 1
 
+    while True:
+        ci, cj = queue.popleft()
+        # 도착 한다면?
+        if ci == n - 1 and cj == m - 1:
+            return visited[ci][cj]
 
-# bfs 정의
-def bfs(s1, s2):
-    # 시작 방문 처리
-    depth[s1][s2] = 1
-    q = deque()
-    q.append((s1, s2))
-
-    # 방문 예약 처리
-    while q:
-        # 꺼내기
-        r, c = q.popleft()
-
-        # 방문 예약
-        for d in range(4):
-            nr = r + dr[d]
-            nc = c + dc[d]
-            # 방문 범위 인지 가능하냐
-            # 방문 처리 및 큐 담기
-            if 0 <= nr < n and 0 <= nc < m and arr[nr][nc] == '1' and depth[nr][nc] == 0 :
-                depth[nr][nc] = depth[r][c] + 1
-                q.append((nr,nc))
+        for ni, nj in ((ci - 1, cj), (ci + 1, cj), (ci, cj - 1), (ci, cj + 1)):
+            if 0 <= ni < n and 0 <= nj < m and not visited[ni][nj] and arr[ni][nj] == '1':
+                visited[ni][nj] = visited[ci][cj] + 1
+                queue.append((ni, nj))
 
 
-bfs(0, 0)
-print(depth[n-1][m-1])
+print(bfs())
