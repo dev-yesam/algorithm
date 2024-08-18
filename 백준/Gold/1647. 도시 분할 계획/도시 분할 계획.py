9e9ca2a63ea392e5169c106ba87ef3f94ff113f5
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-08-18 20:00:30 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1864 ms, Memory: 208396 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -1,52 +1,46 @@
 import sys
+
 input = sys.stdin.readline
 
 
 def find_set(x):
-    if x == p[x]:
+    if p[x] == x:
         return x
-
-    else:
-        p[x] = find_set(p[x])
-        return p[x]
+    p[x] = find_set(p[x])
+    return p[x]
 
 
-def union(x, y):
+def union_set(x, y):
     x = find_set(x)
     y = find_set(y)
-
-    if x == y:
+    if y == x:
         return
 
-    if x < y:
+    if x > y:
         p[y] = x
     else:
         p[x] = y
 
 
-def kruskal():
-    ans = 0
-    cnt = 0
-    for s, e, w in edges:
-        if find_set(s) != find_set(e):
-            union(s, e)
-            ans += w
-            cnt += 1
-            if cnt == n - 2:
-                return ans
-    return 0
-
 n, m = map(int, input().split())
-edges = []
-p = [i for i in range(n + 1)]
-for _ in range(m):
-    s, e, w = map(int, input().split())
-    edges.append((s, e, w))
-    # mst 만든 다음에 제일 가중치 높은 간선 없애면 되겠네.
-
+edges = [tuple(map(int, input().split())) for _ in range(m)]
 edges.sort(key=lambda x: x[2])
+# 크루스칼로 다 연결한다음에, 맨 마지막 경로만 없애면 되겠는데?
 
-# 크루스칼 써야할 듯.
-ans = kruskal()
+p = [i for i in range(n + 1)]
+cnt = 0
+total_weight = 0
 
-print(ans)
+if n > 2:
+    for a, b, c in edges:
+        if find_set(a) == find_set(b):
+            continue
+
+        union_set(a, b)
+        total_weight += c
+
+        cnt += 1
+        if cnt == n - 2:
+            break
+
+print(total_weight)
